d5f632a87377248f133ae6b9ceecfae403e08907
cags9607 <44386058+cags9607@users.noreply.github.com>
2026-05-19 11:51:37 -0600

    Expand __all__ to include additional imports

--- app_classifier/__init__.py
+++ app_classifier/__init__.py
@@ -1,3 +1,15 @@
-from .inference import AppRiskClassifier
+from .inference import (
+    AppRiskClassifier,
+    MultiAppRiskClassifier,
+    ensure_prediction_id_column,
+    generate_prediction_ids,
+    score_with_models,
+)
 
-__all__ = ["AppRiskClassifier"]
+__all__ = [
+    "AppRiskClassifier",
+    "MultiAppRiskClassifier",
+    "ensure_prediction_id_column",
+    "generate_prediction_ids",
+    "score_with_models",
+]
